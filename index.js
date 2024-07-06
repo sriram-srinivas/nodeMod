de4b1638af67baac4f66d75f8e5b0a2e3f5a8daa
sriram-srinivas <sriram.rsrc@gmail.com>
2024-07-06 18:53:52 +0530

    init

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 
+/*
+login module
+session maintenance
+serverless arch
+aws sdk integration
+
+4. ES6/ES7
+
+5. MongoDB
+
+6. Express
+
+7. Socket.IO
+
+8. JWT Authentication
+
+9. Mongoose
+
+10. File and image uploads
+
+11. Email sending
+
+12. Application deployment with Heroku
+
+15. REST API Design
+
+16. Code testing
+
+17. Debugging
+
+18. Jest
+*/ 
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url);
+  res.end();
+}).listen(1994);
